1276db3090e994ee811e219dd31833b6c64d4d1e
Harwex213 <harwex213@mail.ru>
2021-06-09 14:19:34 +0300

    refactor(header): more clean code

--- Simple-Singolo/project/blocks/common/header/header.js
+++ Simple-Singolo/project/blocks/common/header/header.js
@@ -1,54 +1,57 @@
-class Index {
-
-    IndexOverflowHidden() {
-        document.querySelector(".index").classList.add("index_overflow_hidden");
-    }
-
-    IndexOverflowAuto() {
-        document.querySelector(".index").classList.remove("index_overflow_hidden");
-    }
-}
-
 class Header {
-    header = document.querySelector(".header");
-    navbar = document.querySelector(".header__navbar");
-    navbarMobileBackground = document.querySelector(".header__navbar-mobile-bg");
-    navbarBurger = document.querySelector(".header__navbar-burger");
-    logo = document.querySelector(".header__logo");
+    _header
+    _navbar
+    _navbarMobileBackground
+    _navbarBurger
+    _logo
+
+    constructor(headerSelectorName) {
+        this._header = document.querySelector(headerSelectorName);
+        if (this._header === null) {
+            return;
+        }
+
+        this._navbar = this._header.querySelector(".header__navbar");
+        this._navbarMobileBackground = this._header.querySelector(".header__navbar-mobile-bg");
+        this._navbarBurger = this._header.querySelector(".header__navbar-burger");
+        this._logo = this._header.querySelector(".header__logo");
+
+        this._Init();
+    }
+
+    _Init() {
+        this._navbarMobileBackground.addEventListener("click", () => this.ActionWithMobileHeader());
+        this._navbarBurger.addEventListener("click", () => this.ActionWithMobileHeader())
+    }
+
+    ActionWithMobileHeader() {
+        this.RotateBurger() ? this.HideMobileBar() : this.ShowMobileBar();
+    }
 
     RotateBurger() {
-        let isRotated = this.navbarBurger.classList.contains("header__navbar-burger_rotated");
+        let isRotated = this._navbarBurger.classList.contains("header__navbar-burger_rotated");
         if (isRotated)
-            this.navbarBurger.classList.remove("header__navbar-burger_rotated");
+            this._navbarBurger.classList.remove("header__navbar-burger_rotated");
         else
-            this.navbarBurger.classList.add("header__navbar-burger_rotated");
+            this._navbarBurger.classList.add("header__navbar-burger_rotated");
         return isRotated;
     }
 
     ShowMobileBar() {
         new Index().IndexOverflowHidden();
-        this.header.classList.add("header_overflow_hidden");
-        this.navbar.classList.add("header__navbar_visible");
-        this.navbarMobileBackground.classList.add("header__navbar-mobile-bg_visible");
-        this.logo.classList.add("header__logo_pos_left");
+
+        this._header.classList.add("header_overflow_hidden");
+        this._navbar.classList.add("header__navbar_visible");
+        this._navbarMobileBackground.classList.add("header__navbar-mobile-bg_visible");
+        this._logo.classList.add("header__logo_pos_left");
     }
 
     HideMobileBar() {
         new Index().IndexOverflowAuto();
-        this.header.classList.remove("header_overflow_hidden");
-        this.navbar.classList.remove("header__navbar_visible");
-        this.navbarMobileBackground.classList.remove("header__navbar-mobile-bg_visible");
-        this.logo.classList.remove("header__logo_pos_left")
-    }
 
-    constructor() {
-        function Action(headerClass) {
-            headerClass.RotateBurger() ? headerClass.HideMobileBar() : headerClass.ShowMobileBar();
-        }
-
-        this.navbarMobileBackground.addEventListener("click", () => Action(this));
-        this.navbarBurger.addEventListener("click", () => Action(this))
+        this._header.classList.remove("header_overflow_hidden");
+        this._navbar.classList.remove("header__navbar_visible");
+        this._navbarMobileBackground.classList.remove("header__navbar-mobile-bg_visible");
+        this._logo.classList.remove("header__logo_pos_left")
     }
 }
-
-new Header();
