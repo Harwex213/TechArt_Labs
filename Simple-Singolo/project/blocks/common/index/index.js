37c597bc6ba3333b0a7f7952984e8cd62ecaef00
Harwex213 <harwex213@mail.ru>
2021-06-11 12:48:57 +0300

    test(index): successful test - adding slider copy at the index

--- Simple-Singolo/project/blocks/common/index/index.js
+++ Simple-Singolo/project/blocks/common/index/index.js
@@ -12,3 +12,4 @@ const navbar = new Navbar(".header__navbar");
 const header = new Header(".index__header");
 navbar.Attach(header);
 const slider = new Slider(".index__slider", true, true);
+const slider2 = new Slider(".index__slider2", true, true);
