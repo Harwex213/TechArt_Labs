b4bdde95faaf157fecf5c9eeae550b227a71de12
Harwex213 <harwex213@mail.ru>
2021-06-21 16:06:31 +0300

    feat(task4): async/await task done

--- JS_Labs/Task4/main.js
+++ JS_Labs/Task4/main.js
@@ -48,3 +48,43 @@ function makeSomething() {
 makeSomething()
     .then(() => console.log("Less than 2s"))
     .catch(() => console.error("More than 2s"));
+
+// Task D
+class HttpError extends Error {
+    constructor(response) {
+        super(`${response.status} for ${response.url}`);
+        this.name = "HttpError";
+        this.response = response;
+    }
+}
+
+async function loadJson(url) {
+    let response = await fetch(url);
+    if (response.status === 200) {
+        return response.json();
+    } else {
+        throw new HttpError(response);
+    }
+}
+
+async function demoGithubUser() {
+    let name;
+    let user;
+
+    do {
+        name = prompt("Login?", "iliakan");
+        try {
+            user = await loadJson(`https://api.github.com/users/${name}`);
+            alert(`Full name: ${user.name}.`);
+            return user;
+        } catch (error) {
+            if (error instanceof HttpError && error.response.status === 404) {
+                alert("We can’t find such user.");
+            } else {
+                throw error;
+            }
+        }
+    } while (true);
+}
+
+demoGithubUser();
