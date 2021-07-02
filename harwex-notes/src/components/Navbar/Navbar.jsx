2b93dd04f229d46ad6fd462dbe9141efc58045c8
Harwex213 <harwex213@mail.ru>
2021-07-02 14:13:48 +0300

    refactor(react_task1): more independent navbar component

--- harwex-notes/src/components/Navbar/Navbar.jsx
+++ harwex-notes/src/components/Navbar/Navbar.jsx
@@ -1,58 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Layout, Menu, Button } from "antd";
+import { Menu } from "antd";
 import {
     FileOutlined,
     ShareAltOutlined,
     InfoCircleOutlined,
-    LeftOutlined,
 } from "@ant-design/icons";
-
-const { Sider } = Layout;
 
 function Navbar(props) {
     let menuItemId = 0;
     return (
-        <Sider
-            className={props.className}
-            theme="light"
-            collapsed
-            collapsedWidth={50}
-        >
-            <Button
-                shape="circle"
-                icon={<LeftOutlined />}
-                style={{
-                    display: "flex",
-                    margin: "8px auto",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-            />
-            <Menu
-                mode="inline"
-                defaultSelectedKeys={["0"]}
-                style={{
-                    borderRight: "none",
-                }}
-            >
-                <Menu.Item key={menuItemId} icon={<FileOutlined />}>
-                    My Notes
-                </Menu.Item>
-                <Menu.Item key={++menuItemId} icon={<ShareAltOutlined />}>
-                    Shared Notes
-                </Menu.Item>
-                <Menu.Item key={++menuItemId} icon={<InfoCircleOutlined />}>
-                    About
-                </Menu.Item>
-            </Menu>
-        </Sider>
+        <Menu mode="inline" defaultSelectedKeys={["0"]} style={props.style}>
+            <Menu.Item key={menuItemId} icon={<FileOutlined />}>
+                My Notes
+            </Menu.Item>
+            <Menu.Item key={++menuItemId} icon={<ShareAltOutlined />}>
+                Shared Notes
+            </Menu.Item>
+            <Menu.Item key={++menuItemId} icon={<InfoCircleOutlined />}>
+                About
+            </Menu.Item>
+        </Menu>
     );
 }
 
 Navbar.propTypes = {
     className: PropTypes.string,
+    style: PropTypes.object,
 };
 
 export default Navbar;
