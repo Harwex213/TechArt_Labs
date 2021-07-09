f8af5f5c8375279cb1ce6942e074de2b45e185ab
Harwex213 <harwex213@mail.ru>
2021-07-09 13:24:16 +0300

    fix(react-task3/App): change selected link in menu

--- harwex-notes/src/app/App.js
+++ harwex-notes/src/app/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect, Link } from "react-router-dom";
+import { Switch, Route, Redirect, Link, useLocation } from "react-router-dom";
 
 import { Layout, Menu } from "antd";
 import { FileOutlined, InfoCircleOutlined, ShareAltOutlined } from "@ant-design/icons";
@@ -7,45 +7,61 @@ import { FileOutlined, InfoCircleOutlined, ShareAltOutlined } from "@ant-design/
 import MyNotesPage from "../pages/myNotes/myNotesPage";
 import SharedNotesPage from "../pages/sharedNotes/sharedNotesPage";
 import AboutPage from "../pages/about/aboutPage";
+import NotFoundPage from "../pages/notFound/notFoundPage";
 
 import Styles from "./styled";
-import NotFoundPage from "../pages/notFound/notFoundPage";
 
 const { Header, Content } = Layout;
 
+const menuLinks = new Map([
+    ["/notes", "0"],
+    ["/shared-notes", "1"],
+    ["/about", "2"],
+    ["/not-found", "3"],
+]);
+const menuLinkPaths = [...menuLinks.keys()];
+
 const App = () => {
+    let location = useLocation();
+    let selectedKey = menuLinks.get(location.pathname);
+
     return (
         <Layout style={Styles.app}>
             <Header style={Styles.app__header}>
                 <h1 style={Styles.header__title}>Harwex Notes</h1>
-                <Menu style={Styles.header__menu} mode="horizontal" theme="light" defaultSelectedKeys={["0"]}>
+                <Menu
+                    style={Styles.header__menu}
+                    mode="horizontal"
+                    theme="light"
+                    selectedKeys={[selectedKey]}
+                >
                     <Menu.Item key={0} icon={<FileOutlined />}>
-                        <Link to="/notes">My Notes</Link>
+                        <Link to={menuLinkPaths[0]}>My Notes</Link>
                     </Menu.Item>
                     <Menu.Item key={1} icon={<ShareAltOutlined />}>
-                        <Link to="/shared-notes">Shared Notes</Link>
+                        <Link to={menuLinkPaths[1]}>Shared Notes</Link>
                     </Menu.Item>
                     <Menu.Item key={2} icon={<InfoCircleOutlined />}>
-                        <Link to="/about">About</Link>
+                        <Link to={menuLinkPaths[2]}>About</Link>
                     </Menu.Item>
                 </Menu>
             </Header>
             <Content style={Styles.app__content}>
                 <Switch>
-                    <Route path="/notes">
+                    <Route path={menuLinkPaths[0]}>
                         <MyNotesPage />
                     </Route>
-                    <Route path="/shared-notes">
+                    <Route path={menuLinkPaths[1]}>
                         <SharedNotesPage />
                     </Route>
-                    <Route path="/about">
+                    <Route path={menuLinkPaths[2]}>
                         <AboutPage />
                     </Route>
-                    <Route path="/not-found">
+                    <Route path={menuLinkPaths[3]}>
                         <NotFoundPage />
                     </Route>
                     <Route path="*">
-                        <Redirect to="/not-found" />
+                        <Redirect to={menuLinkPaths[3]} />
                     </Route>
                 </Switch>
             </Content>
