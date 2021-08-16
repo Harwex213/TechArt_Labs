15a88b444245733e5fd0ef354437bfe80eb26195
Harwex213 <harwex213@mail.ru>
2021-08-16 14:43:12 +0300

    feat(reg): static version of reg

--- harwex-tickets/src/pages/Registration/RegistrationPage.jsx
+++ harwex-tickets/src/pages/Registration/RegistrationPage.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
+import { Formik } from "formik";
+import { Form, Input, SubmitButton, DatePicker } from "formik-antd";
+import * as Yup from "yup";
+
+import styles from "./styles";
+
+const registrationSchema = Yup.object().shape({
+    username: Yup.string().min(4, "Too Short!").max(50, "Too Long!").required("Required"),
+    firstname: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    lastname: Yup.string().min(4, "Too Short!").max(50, "Too Long!").required("Required"),
+    phoneNumber: Yup.string().required("Required"),
+    dateOfBirth: Yup.date().required("Required"),
+    password: Yup.string().min(8, "Too Short!").max(50, "Too Long!").required("Required"),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password")], "Passwords must match")
+        .required("Please, repeat your password"),
+});
+
 const RegistrationPage = () => {
-    return <h1>Registration</h1>;
+    const handleSubmit = async (values, _) => {
+        console.log("submit");
+    };
+
+    return (
+        <div style={styles.layout}>
+            <Formik
+                initialValues={{
+                    username: "",
+                    firstname: "",
+                    lastname: "",
+                    dateOfBirth: "",
+                    phoneNumber: "",
+                    password: "",
+                    confirmPassword: "",
+                }}
+                validationSchema={registrationSchema}
+                onSubmit={handleSubmit}
+            >
+                <Form>
+                    <Form.Item name="username">
+                        <Input name="username" placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item name="firstname">
+                        <Input name="firstname" placeholder="Firstname" />
+                    </Form.Item>
+                    <Form.Item name="lastname">
+                        <Input name="lastname" placeholder="Lastname" />
+                    </Form.Item>
+                    <Form.Item name="dateOfBirth">
+                        <DatePicker name="dateOfBirth" placeholder="Birthdate" />
+                    </Form.Item>
+                    <Form.Item name="phoneNumber">
+                        <Input name="phoneNumber" placeholder="Phone Number" />
+                    </Form.Item>
+                    <Form.Item name="password">
+                        <Input.Password name="password" placeholder="Password" />
+                    </Form.Item>
+                    <Form.Item name="confirmPassword">
+                        <Input.Password name="confirmPassword" placeholder="Confirm Password" />
+                    </Form.Item>
+
+                    <SubmitButton>Register</SubmitButton>
+                </Form>
+            </Formik>
+        </div>
+    );
 };
 
 export default RegistrationPage;
